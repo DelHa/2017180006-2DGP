75014fdc642f6a75f635458cbcafd4358a53de86
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-09-18 17:49:36 +0900

    Drill05 코드의 기본 구조 완성
    코드의 기본적인 뼈대를 완성했다.
    함수 이름은 항상 동사로 시작되어야한다.

--- Drills/Drill05/Drill-05.py
+++ Drills/Drill05/Drill-05.py
@@ -5,6 +5,21 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# fill here
+# 0918메모
+# 함수를 사용하면 코드를 읽기가 쉬워진다.
+# 함수 이름은 '항상 동사'로 시작되어야한다.
+
+def make_rectangle():
     
+    pass
+def make_circle():
+
+    pass
+
+while True:
+    make_rectangle()
+    make_circle()
+
+
+
 close_canvas()
