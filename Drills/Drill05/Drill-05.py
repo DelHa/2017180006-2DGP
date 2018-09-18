0b08af866c3946feceb273a0b27f019a6d16c98b
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-09-18 18:01:43 +0900

    Drill05 오른쪽으로 가는 코드 완성
    오른쪽으로 가는 코드를 완성했습니다.

--- Drills/Drill05/Drill-05.py
+++ Drills/Drill05/Drill-05.py
@@ -9,8 +9,16 @@ character = load_image('character.png')
 # 함수를 사용하면 코드를 읽기가 쉬워진다.
 # 함수 이름은 '항상 동사'로 시작되어야한다.
 # 함수를 적절하게 사용하면 코더의 의도가 보인다.
+# 800//2 -> 화면의 중심으로 이동시키는것을 바로 알아볼 수 있다.
 def move_from_center_to_right():
-    pass
+    x, y = 800 // 2, 90
+    while x < 800 - 25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
+
 def move_up():
     pass
 def move_left():
