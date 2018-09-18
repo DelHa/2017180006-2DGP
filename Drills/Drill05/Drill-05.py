364f623850b4b972b899ffab18a970f01f923f99
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-09-18 18:41:31 +0900

    Drill05 지정된 경로 좌표 입력
    지정된 경로 좌표를 입력했습니다.
    03

--- Drills/Drill05/Drill-05.py
+++ Drills/Drill05/Drill-05.py
@@ -3,7 +3,20 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
+
+#지정된 경로좌표
+pos1_x, pos1_y = 203, 535
+pos2_x, pos2_y = 132, 243
+pos3_x, pos3_y = 535, 470
+pos4_x, pos4_y = 477, 203
+pos5_x, pos5_y = 715, 136
+pos6_x, pos6_y = 316, 225
+pos7_x, pos7_y = 510, 92
+pos8_x, pos8_y = 692, 518
+pos9_x, pos9_y = 682, 336
+pos10_x, pos10_y = 712, 349
+
 
 # 0918메모
 # 함수를 사용하면 코드를 읽기가 쉬워진다.
@@ -12,16 +25,19 @@ character = load_image('character.png')
 # 800//2 -> 화면의 중심으로 이동시키는것을 바로 알아볼 수 있다.
 # 코드가 실행이 가능할때 commit 한다.
 def look_chart():
-    pass
-def move_chart():
-    pass
-
-def move_charater():
+    character.clip_draw()
+def move_left_chart():
     look_chart()
-    move_chart()
+
+
+def move_right_chart():
+    look_chart()
+
+
 
 while True:
-    move_charater()
+    move_left_chart()
+    move_right_chart()
 
 
 close_canvas()
