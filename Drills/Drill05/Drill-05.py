f79ed13bc58c595333245221c11a80b5485dee6c
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-09-18 17:55:36 +0900

    Drill05 사각형을 그리는 구체적인 알고리즘 완성
    사각형을 그리는 뼈대를 완성했다.
    함수를 적절하게 사용하면 코더의 의도가 보인다.

--- Drills/Drill05/Drill-05.py
+++ Drills/Drill05/Drill-05.py
@@ -8,10 +8,27 @@ character = load_image('character.png')
 # 0918메모
 # 함수를 사용하면 코드를 읽기가 쉬워진다.
 # 함수 이름은 '항상 동사'로 시작되어야한다.
+# 함수를 적절하게 사용하면 코더의 의도가 보인다.
+def move_from_center_to_right():
+    pass
+def move_up():
+    pass
+def move_left():
+    pass
+def move_down():
+    pass
+def move_left_to_center():
+    pass
+
 
 def make_rectangle():
-    
-    pass
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_left_to_center()
+
+
 def make_circle():
 
     pass
