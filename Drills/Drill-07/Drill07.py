def1c13f29943081115594a05c046de8c3734831
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-10-02 19:06:43 +0900

    Drill07
    Drill 07
    2017180006 김지영
    유튜브 링크
    [김지영] [오후 7:05] https://youtu.be/Mx6-_iyfkrM

--- Drills/Drill-07/Drill07.py
+++ Drills/Drill-07/Drill07.py
@@ -12,45 +12,6 @@ def stop():
     turtle.bye()
 
 
-def prepare_turtle_canvas():
-    turtle.setup(1024, 768)
-    turtle.bgcolor(0.2, 0.2, 0.2)
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.shape('arrow')
-    turtle.shapesize(2)
-    turtle.pensize(5)
-    turtle.color(1, 0, 0)
-    turtle.speed(100)
-    turtle.goto(-500, 0)
-    turtle.pendown()
-    turtle.goto(480, 0)
-    turtle.stamp()
-    turtle.penup()
-    turtle.goto(0, -360)
-    turtle.pendown()
-    turtle.goto(0, 360)
-    turtle.setheading(90)
-    turtle.stamp()
-    turtle.penup()
-    turtle.home()
-
-    turtle.shape('circle')
-    turtle.pensize(1)
-    turtle.color(0, 0, 0)
-    turtle.speed(50)
-
-    turtle.onkey(stop, 'Escape')
-    turtle.listen()
-
-
-def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
-
-
 def draw_point(p):
     turtle.goto(p)
     #turtle.dot(5, random.random(), random.random(), random.random())
@@ -111,11 +72,6 @@ def draw_line(p1, p2):
     update_canvas()
     frame = (frame + 1) % 8
 
-
-#prepare_turtle_canvas()
-
-
-#points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
 
 size = 20
 points = [(random.randint(100 , KPU_WIDTH - 100), random.randint(100,KPU_HEIGHT - 100)) for i in range(size)]
