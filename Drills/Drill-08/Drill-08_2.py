81ff3c73d287be9d9b485025197a92ee1c0f08b0
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-10-04 18:35:25 +0900

    2-02
    수정

--- Drills/Drill-08/Drill-08_2.py
+++ Drills/Drill-08/Drill-08_2.py
@@ -12,6 +12,12 @@ def stop():
     turtle.bye()
 
 
+look = 1
+frame = 0
+running = True
+again_click = False
+character = load_image('animation_sheet.png')
+
 def prepare_turtle_canvas():
     turtle.setup(1024, 768)
     turtle.bgcolor(0.2, 0.2, 0.2)
@@ -60,44 +66,75 @@ def draw_point(p):
 
 
 def draw_curve_4_points(p1, p2, p3, p4):
-    draw_big_point(p1)
-    draw_big_point(p2)
-    draw_big_point(p3)
-    draw_big_point(p4)
+    global look
+    global frame
+    global again_click
+    global x
+    global y
 
     # draw p1-p2
-    for i in range(0, 50, 4):
+    for i in range(0, 50, 1):
         t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-        draw_point((x, y))
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+
+        if p1[0] > p2[0]:
+            look = 0
+        else:
+            look = 1
+
+        character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
     draw_point(p2)
 
     # draw p2-p3
-    for i in range(0, 100, 4):
+    for i in range(0, 100, 1):
         t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        draw_point((x, y))
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+
+        if p2[0] > p3[0]:
+            look = 0
+        else:
+            look = 1
+
+        character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
     draw_point(p3)
 
     # draw p3-p4
-    for i in range(50, 100, 4):
+    for i in range(0, 100, 1):
         t = i / 100
-        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
-        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
-        draw_point((x, y))
-    draw_point(p4)
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
 
-    # draw p3-p4
-    for i in range(0, 100, 4):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+        if p3[0] > p4[0]:
+            look = 0
+        else:
+            look = 1
 
-        draw_point((x, y))
+        character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
     draw_point(p1)
 
 
