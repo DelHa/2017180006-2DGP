46cc7205068faaf314f3dd59a4bf43d51282334f
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-10-04 18:33:11 +0900

    2-01
    기존의 Drill08 - 1에 이미지 추가

--- Drills/Drill-08/Drill-08_2.py
+++ Drills/Drill-08/Drill-08_2.py
@@ -12,95 +12,98 @@ def stop():
     turtle.bye()
 
 
-look = 1
-frame = 0
-running = True
-again_click = False
-character = load_image('animation_sheet.png')
+def prepare_turtle_canvas():
+    turtle.setup(1024, 768)
+    turtle.bgcolor(0.2, 0.2, 0.2)
+    turtle.penup()
+    turtle.hideturtle()
+    turtle.shape('arrow')
+    turtle.shapesize(2)
+    turtle.pensize(5)
+    turtle.color(1, 0, 0)
+    turtle.speed(100)
+    turtle.goto(-500, 0)
+    turtle.pendown()
+    turtle.goto(480, 0)
+    turtle.stamp()
+    turtle.penup()
+    turtle.goto(0, -360)
+    turtle.pendown()
+    turtle.goto(0, 360)
+    turtle.setheading(90)
+    turtle.stamp()
+    turtle.penup()
+    turtle.home()
+
+    turtle.shape('circle')
+    turtle.pensize(1)
+    turtle.color(0, 0, 0)
+    turtle.speed(50)
+
+    turtle.onkey(stop, 'Escape')
+    turtle.listen()
 
 
-def draw_curve_3_points(p1, p2, p3, p4):
-    global look
-    global frame
-    global again_click
-    global x
-    global y
+
+def draw_big_point(p):
+    turtle.goto(p)
+    turtle.color(0.8, 0.9, 0)
+    turtle.dot(15)
+    turtle.write('     '+str(p))
 
 
-    for i in range(0, 50, 1):
+def draw_point(p):
+    turtle.goto(p)
+    turtle.dot(5, random.random(), random.random(), random.random())
+
+
+
+
+def draw_curve_4_points(p1, p2, p3, p4):
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+    draw_big_point(p4)
+
+    # draw p1-p2
+    for i in range(0, 50, 4):
         t = i / 100
         x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
         y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-
-
-        clear_canvas()
-        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-
-        if p1[0] > p2[0]:
-            look = 0
-        else:
-            look = 1
-
-        character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
-        update_canvas()
-        frame = (frame + 1) % 8
-        #draw_point((x, y))
-    #draw_point(p2)
+        draw_point((x, y))
+    draw_point(p2)
 
     # draw p2-p3
-    for i in range(0, 100, 1):
+    for i in range(0, 100, 4):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        clear_canvas()
-        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-
-        if p2[0] > p3[0]:
-            look = 0
-        else:
-            look = 1
-
-        character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
-        update_canvas()
-        frame = (frame + 1) % 8
-        #draw_point((x, y))
-    #draw_point(p3)
+        draw_point((x, y))
+    draw_point(p3)
 
     # draw p3-p4
-    for i in range(0, 100, 1):
+    for i in range(50, 100, 4):
         t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
-        clear_canvas()
-        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
+        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
+        draw_point((x, y))
+    draw_point(p4)
 
-        if p3[0] > p4[0]:
-            look = 0
-        else:
-            look = 1
+    # draw p3-p4
+    for i in range(0, 100, 4):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
 
-        character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
-        update_canvas()
-        frame = (frame + 1) % 8
-
-    clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * look, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
+        draw_point((x, y))
+    draw_point(p1)
 
 
-size = 20
-points = [(random.randint(300 , KPU_WIDTH - 100), random.randint(300,KPU_HEIGHT - 100)) for i in range(size)]
-
-n = 3
+prepare_turtle_canvas()
 
 while True:
-    draw_curve_3_points(points[n-3],points[n-2],points[n-1], points[n])
-    n = (n+3) % size
-
+    draw_curve_4_points((-300, 200), (400, 350), (300, -300), (-200, -200))
 
 turtle.done()
-close_canvas()
