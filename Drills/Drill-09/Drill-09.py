d719b09fb7ffc3a196fcdb88c32a9a7393970537
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-10-09 18:41:15 +0900

    Drill09-04
    소년 수정

--- Drills/Drill-09/Drill-09.py
+++ Drills/Drill-09/Drill-09.py
@@ -10,11 +10,13 @@ class Grass:
 class Boy:
     def __init__(self):
         self.x , self.y = 0,90
+        self.x , self.y = random.randint(100,700), 90
         self.frame = 0
         self.image = load_image('run_animation.png')
 
     def update(self):
         self.frame = (self.frame + 1)% 8
+        self.frame = random.randint(1,8)
         self.x += 5
     def draw(self):
         self.image.clip_draw(self.frame * 100 , 0,100,100,self.x, self.y)
