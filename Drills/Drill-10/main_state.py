e69e261cbe150c7565650bdcaed53e93642e5ed4
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-10-11 19:05:26 +0900

    Drill10_02를 위한 파일수정
    파일수정

--- Drills/Drill-10/main_state.py
+++ Drills/Drill-10/main_state.py
@@ -6,7 +6,7 @@ from pico2d import *
 
 import game_framework
 import title_state
-import basic_pause_state
+import second_pause_state
 
 
 name = "MainState"
@@ -76,7 +76,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.push_state(basic_pause_state)
+            game_framework.push_state(second_pause_state)
     pass
 
 
