d484d589263529a3a4b1ecd457f651ba604a3150
Delmonte_JY <39023756+KimJiYeong@users.noreply.github.com>
2018-10-11 18:41:17 +0900

    Drill10 01
    pausepy credit  modify

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -1,14 +1,14 @@
 import game_framework
 from pico2d import *
 import main_state
-
+import main_state
 name = "TitleState"
 image = None
 
 
 def enter():
     global image
-    image = load_image('title.png')
+    image = load_image('pause.png')
     pass
 
 def exit():
@@ -24,12 +24,13 @@ def handle_events():
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
-            elif (event.type, event.key ) == (SDL_KEYDOWN, SDLK_SPACE):
+            elif (event.type, event.key ) == (SDL_KEYDOWN, SDLK_p):
                 game_framework.change_state(main_state)
     pass
 
 def draw():
     clear_canvas()
+    main_state.draw()
     image.draw(400, 300)
     update_canvas()
     pass
